609498657a2739147b73258a2b34d6fbf39ccd78
anumsameer <anumsameer@msi.ben.pri>
2022-11-16 12:20:25 -0600

    Anum Sameer PE6.4v2

--- AnumSameer_2200_PE6.4v2/src/ExamPractice06_04v2.java
+++ AnumSameer_2200_PE6.4v2/src/ExamPractice06_04v2.java
@@ -1,11 +1,11 @@
-/***************************************************************
- * 
- * @author Anum Sameer
+ /***************************************************************
+* @author Anum Sameer
+
  * Date: 11/16/2022 [W]
  * 
  ***************************************************************/
 import java.util.Scanner;
-public class Exercise06_04 {
+public class ExamPractice06_04v2 {
 	public static void main(String[] args) {
 		System.out.print("Enter an integer: ");
 		Scanner input = new Scanner(System.in);
