b7cf89d237c422b1621760dd4df601ace2ae7209
anumsameer <anumsameer@localhost>
2022-11-18 11:03:53 -0600

    AnumSameerPE6.4_Friday_11182022

---
+++ AnumSameer_2200_PE6.4v2/src/Exercise06_04.java
@@ -0,0 +1,34 @@
+/************************
+ * 
+ * @author Anum Sameer 
+ * @date 11/14/2022 [M]
+ * 		 11/18/2022 [F] Modify solution from last Monday
+ *
+ */
+import java.util.Scanner;
+public class Exercise06_04 {
+	public static void main(String[] args) {
+	System.out.print("Enter an integer: ");
+	Scanner input = new Scanner(System.in);
+	int number = input.nextInt();
+	reverse(number);
+	input.close();
+	
+	
+	
+	
+	
+}
+public static void reverse(int n) {
+	if(n == 0) {
+		System.out.print(n);
+	}
+	if( n < 0) {
+		System.out.print("-");
+		n = (-1) * n;
+	}
+	while( n!= 0) {
+		System.out.print(n % 10);
+		n = n / 10;
+		}
+	}                      										}
